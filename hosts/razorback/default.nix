8e507c11146cc3da8792ae7b40cc062c34ebdd3f
László Vaskó <laszlo.vasko@outlook.com>
2021-08-08 09:16:57 +0200

    razorback: enable absent modules

--- hosts/razorback/default.nix
+++ hosts/razorback/default.nix
@@ -21,10 +21,12 @@
     isAdmin = true;
     uid = 1000;
     home-manager = {
+      _.development.enable = true;
       _.git.enable = true;
       _.gpg.enable = true;
       _.tools.enable = true;
       _.emacs.enable = true;
+      _.awesome.enable = true;
     };
   };
 }
