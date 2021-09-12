77b94af196ca84f6bd40cbacdfede373db2217b6
László Vaskó <laszlo.vasko@outlook.com>
2021-09-13 00:14:52 +0200

    emacs: set `org-agenda-files`

--- modules/home/emacs.nix
+++ modules/home/emacs.nix
@@ -7,6 +7,7 @@ in lib.mkProfile "emacs" {
     settings = {
       org-journal-dir = "~/devel/git/github.com/vlaci/notes/org/journal";
       org-roam-directory = "~/devel/git/github.com/vlaci/notes/org/roam";
+      org-agenda-files = [ "~/devel/git/github.com/vlaci/notes/org" ];
       projectile-project-search-path = [
         { elisp = ''("~/devel/git" . 2)''; }
       ];
