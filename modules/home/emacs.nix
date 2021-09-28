4d35899ed96c2e23caf466f061584f99f2ff66bd
László Vaskó <laszlo.vasko@outlook.com>
2021-09-28 23:59:08 +0200

    emacs: set org subdirectories from `init.el`

--- modules/home/emacs.nix
+++ modules/home/emacs.nix
@@ -5,9 +5,7 @@ in lib.mkProfile "emacs" {
   emacsVlaci = {
     enable = true;
     settings = {
-      org-journal-dir = "~/devel/git/github.com/vlaci/notes/org/journal";
-      org-roam-directory = "~/devel/git/github.com/vlaci/notes/org/roam";
-      org-agenda-files = [ "~/devel/git/github.com/vlaci/notes/org" ];
+      org-directory = "~/devel/git/github.com/vlaci/notes/org";
       projectile-project-search-path = [
         { elisp = ''("~/devel/git" . 2)''; }
       ];
