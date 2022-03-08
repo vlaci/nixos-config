29ec7f12ba505710720516ef57503cad1ecda6ee
László Vaskó <1771332+vlaci@users.noreply.github.com>
2022-03-08 23:28:52 +0100

    development: refresh package list

--- modules/home/development/default.nix
+++ modules/home/development/default.nix
@@ -3,10 +3,24 @@
 lib.mkProfile "development" {
   home.enableDebugInfo = true;
   home.packages = with pkgs; [
-    vscode
+    julia-bin
+    (vscode.fhsWithPackages (pkgs: with pkgs; [
+      zsh
+      gcr liburcu openssl krb5 zlib icu gnome3.gnome-keyring libsecret desktop-file-utils xorg.xprop
+      openssl.dev
+      pkg-config
+      binutils
+      zlib.dev
+      neovim
+      gcc
+    ]))
     openssl
     pandoc
     rnix-lsp
+    cargo-edit
+    clang
+    lldb
+    nodejs
     rustup
     rust-analyzer
     mitmproxy
