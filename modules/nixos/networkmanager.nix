71c0c0cdfb494796fcfc1823ee13b4c4dcb91c83
László Vaskó <1771332+vlaci@users.noreply.github.com>
2022-04-13 16:07:41 +0200

    networkmanager: follow-up breaking change

--- modules/nixos/networkmanager.nix
+++ modules/nixos/networkmanager.nix
@@ -5,10 +5,11 @@ lib.mkProfile "networkmanager" {
     enable = true;
     dhcp = "dhcpcd";
     dns = "dnsmasq";
-
-    packages = with pkgs; ([
-      pkgs.dhcpcd
-    ] ++ lib.optionals config._.gui.enable [ networkmanagerapplet ]);
   };
+
+  environment.systemPackages = with pkgs; ([
+    pkgs.dhcpcd
+  ] ++ lib.optionals config._.gui.enable [ networkmanagerapplet ]);
+
   _.users.defaultGroups = [ "networkmanager" ];
 }
