67561fbf25dde970ca500b6fe105ef785d067a00
László Vaskó <1771332+vlaci@users.noreply.github.com>
2022-08-18 18:04:25 +0200

    cachix: use unblob cache

---
+++ modules/nixos/cachix/cachix/unblob.nix
@@ -0,0 +1,10 @@
+{
+  nix.settings = {
+    substituters = [
+      "https://unblob.cachix.org"
+    ];
+    trusted-public-keys = [
+      "unblob.cachix.org-1:5kWA6DwOg176rSqU8TOTBXWxsDB4LoCMfGfTgL5qCAE="
+    ];
+  };
+}
