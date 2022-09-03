c1893b0301efee663ac3680775411a3651da72da
László Vaskó <1771332+vlaci@users.noreply.github.com>
2022-09-03 23:38:04 +0200

    wayland: reformat

--- modules/nixos/gui/wayland.nix
+++ modules/nixos/gui/wayland.nix
@@ -10,15 +10,16 @@ let
     exec "GTK_DATA_PREFIX=${theme.package} GTK_THEME=${theme.name} ${pkgs.greetd.gtkgreet}/bin/gtkgreet -l; ${pkgs.sway}/bin/swaymsg exit"
 
     bindsym Mod4+shift+e exec swaynag \
-	    -t warning \
-	    -m 'What do you want to do?' \
-	    -b 'Poweroff' 'systemctl poweroff' \
-	    -b 'Reboot' 'systemctl reboot'
+      -t warning \
+      -m 'What do you want to do?' \
+      -b 'Poweroff' 'systemctl poweroff' \
+      -b 'Reboot' 'systemctl reboot'
 
     #include /etc/sway/config.d/*
   '';
   theme = config._.theme.gtkTheme;
-in {
+in
+{
   options = {
     _.gui.wayland.enable = mkEnableOption "wayland";
   };
