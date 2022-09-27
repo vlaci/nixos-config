933dc1bd15bd5556c75f3002952e9ccf084c284a
László Vaskó <1771332+vlaci@users.noreply.github.com>
2022-09-27 12:58:27 +0200

    secrets: mount /home to access ssh keys for agenix

--- modules/nixos/secrets.nix
+++ modules/nixos/secrets.nix
@@ -38,5 +38,8 @@ in
       vlaci = tryImport ../../secrets/vlaci.nix.age;
       work = tryImport ../../secrets/work.nix.age;
     };
+
+    # agenix may use personal SSH private keys
+    fileSystems."/home".neededForBoot = true;
   };
 }
