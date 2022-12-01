8d797d0f2d7ed60f5de1198a615087bbb3b78d70
László Vaskó <1771332+vlaci@users.noreply.github.com>
2022-12-01 10:35:28 +0100

    yubioath-desktop is deprecated

--- modules/nixos/yubikey/default.nix
+++ modules/nixos/yubikey/default.nix
@@ -74,7 +74,7 @@ in
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
       yubikey-personalization
-      yubioath-desktop
+      yubioath-flutter
     ] ++ optionals config._.gui.enable [
       yubikey-personalization-gui
     ];
