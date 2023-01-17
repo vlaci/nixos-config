f3a73c1df27ca22d9bc5e66849a3d32a50bf04ae
László Vaskó <1771332+vlaci@users.noreply.github.com>
2023-01-17 16:09:11 +0100

    slack: override for screen share

--- modules/nixos/gui/wayland.nix
+++ modules/nixos/gui/wayland.nix
@@ -48,9 +48,14 @@ in
 
     nixpkgs.overlays = [
       (self: super: {
-        xdg-desktop-portal-wlr = super.xdg-desktop-portal-wlr.overrideAttrs (super: {
-          postPatch = ''
-            substituteInPlace wlr.portal --replace "sway" "sway;hyprland"
+        slack = super.slack.overrideAttrs (old: {
+          installPhase = old.installPhase + ''
+            rm $out/bin/slack
+
+            makeWrapper $out/lib/slack/slack $out/bin/slack \
+            --prefix XDG_DATA_DIRS : $GSETTINGS_SCHEMAS_PATH \
+            --prefix PATH : ${lib.makeBinPath [pkgs.xdg-utils]} \
+            --add-flags "--enable-features=WebRTCPipeWireCapturer %U"
           '';
         });
       })
