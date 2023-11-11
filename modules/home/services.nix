a2a2f130aa8158650740771200bc1e30437a340a
László Vaskó <1771332+vlaci@users.noreply.github.com>
2023-11-11 12:39:13 +0100

    virt-manager: default configuration

--- modules/home/services.nix
+++ modules/home/services.nix
@@ -6,6 +6,10 @@
       home.packages = with pkgs; [
         virt-manager
       ];
+      dconf.settings."org/virt-manager/virt-manager/connections" = {
+        autoconnect = [ "qemu:///system" ];
+        uris = [ "qemu:///system" ];
+      };
     })
   ];
 }
